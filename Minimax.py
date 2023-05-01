bd21830d58ab9fd3871941be075b8a813fc186a5
Kiran Deep <kirandeep102030@gmail.com>
2023-05-02 02:37:16 +0530

    Added Minimax

---
+++ Minimax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import math
+
+arr = [3,5,2,9,12,5,23,23]
+n = int(math.log2(len(arr)))
+
+def minimax(node, depth):
+    if depth == n:
+        return arr[node]
+    left = minimax(node*2+1, depth+1)
+    right = minimax(node*2+2, depth+1)
+    if depth&1:
+        return min(left, right)
+    else:
+        return max(left, right)
+
+print(minimax(0,1))
