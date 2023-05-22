9ca68e671cac65d96b2cf8981de9aeccf6c1bbf3
Kirandeep2806 <kirandeep102030@gmail.com>
2023-05-22 21:23:30 +0530

    Added Value Iteration

--- ValueIteration.py
+++ ValueIteration.py
@@ -1,43 +1,81 @@
-actions = (0, 1)
-states = (0, 1, 2, 3, 4)
-rewards = [-1, -1, 10, -1, -5]
-gamma = 0.9
+#!/usr/bin/python3
 
-prob = [
-    [[0.9, 0.1], [0.1, 0.9], [0, 0], [0, 0], [0, 0]],
-    [[0.9, 0.1], [0, 0], [0.1, 0.9], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0.9, 0.1], [0, 0], [0.1, 0.9]],
-    [[0, 0], [0, 0], [0, 0], [0.9, 0.1], [0.1, 0.9]],
-]
+REWARD = -0.01
+DISCOUNT = 0.99
+MAX_ERROR = 10**(-3)
+ACTIONS = [(1,0),(-1,0),(0,1),(0,-1)]
+DIRECTIONS = 4
+NUM_ROWS = 3
+NUM_COLS = 4
+V = [[0, 0, 0, 1], [0, 0, 0, -1], [0, 0, 0, 0]]
 
-V = [0, 0, 0, 0, 0]
-pi = [None, None, None, None, None]
+def GetV(V, r, c, action):
+    dr, dc = ACTIONS[action]
+    r += dr
+    c += dc
+    if r<0 or c<0 or r>=NUM_ROWS or c>=NUM_COLS or (r==1 and c==1):
+        return V[r-dr][c-dc]
+    return V[r][c]
 
-while True:
-    V_new = [0, 0, 0, 0, 0]
-    for s in states:
-        max_val = 0
-        for a in actions:
-            val = 0
-            for s_next in states:
-                val += prob[s][s_next][a] * V[s_next]
-            val *= gamma
-            val += rewards[s]
-            max_val = max(max_val, val)
-            if V[s] < val:
-                pi[s] = a
-        V_new[s] = round(max_val, 2)
+def CalculateV(V, r, c, action):
+    v = REWARD
+    v += 0.1 * DISCOUNT * GetV(V, r, c, (action-1)%4)
+    v += 0.8 * DISCOUNT * GetV(V, r, c, action)
+    v += 0.1 * DISCOUNT * GetV(V, r, c, (action+1)%4)
+    return v
 
-    if V_new == V:
-        break
-    V = V_new
+def ValueIteration(V):
+    while True:
+        VDash = [[0, 0, 0, 1], [0, 0, 0, -1], [0, 0, 0, 0]]
+        err = 0
+        for r in range(NUM_ROWS):
+            for c in range(NUM_COLS):
+                if (r<=1 and c==3) or (r==1 and c==1):
+                    continue
+                cv = [CalculateV(V, r, c, action) for action in range(DIRECTIONS)]
+                m = max(cv)
+                VDash[r][c] = round(m, 4)
+                err = max(err, VDash[r][c] - V[r][c])
+        V = VDash
+        if err < MAX_ERROR * (1-DISCOUNT) / DISCOUNT:
+            break
+    return V
 
-print("Best approach at each state :", V)
-print("Optimal policy :", pi)
-for i in range(len(pi)):
-    if pi[i] == 1:
-        pi[i] = "\u2192"
-    if pi[i] == 0:
-        pi[i] = "\u2190"
-print("Optimal policy :", pi)
+def GetOptimalPolicy(V):
+    policy = [[-1, -1, -1, -1] for _ in range(NUM_ROWS)]
+    for r in range(NUM_ROWS):
+        for c in range(NUM_COLS):
+            if (r<=1 and c==3) or (r==1 and c==1):
+                continue
+            maxAction, maxV = None, -float("inf")
+            for action in range(DIRECTIONS):
+                v = CalculateV(V, r, c, action)
+                if v > maxV:
+                    maxAction, maxV = action, v
+            policy[r][c] = maxAction
+    return policy
+
+def PrintEnvironment(V):
+    for r in range(NUM_ROWS):
+        for c in range(NUM_COLS):
+            if r == 1 and c == 1:
+                print("WALL".ljust(6), end=" | ")
+            elif c == 3:
+                if r <= 1:
+                    if r == 0:
+                        print("+1")
+                    else:
+                        print("-1")
+                else:
+                    print(str(V[r][c]).ljust(6))
+            else:
+                print(str(V[r][c]).ljust(6), end=" | ")
+
+print("Initial Environment : \n")
+PrintEnvironment(V)
+
+V = ValueIteration(V)
+res = GetOptimalPolicy(V)
+
+print("\n\nOptimal Policy : \n")
+PrintEnvironment(V)
